11ec39329fd12e4858f381b76d0b29a82a0f19e3
VladimirOstr <vovuchaostrovsky@gmail.com>
2022-04-24 21:27:29 +0700

    добавил классы

---
+++ src/ContactsApp/ContactsApp.Model/PhoneNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp.Model
+{
+    /// <summary>
+    /// Номер телефона.
+    /// </summary>
+    class PhoneNumber
+    {
+		/// <summary>
+		/// Номер телефона.
+		/// </summary>
+		private long _number;
+
+		/// <summary>
+		/// Максимальное количетсов цифр в номере.
+		/// </summary>
+		public const int MAXDIGITCOUNT = 11;
+
+		/// <summary>
+		/// Возвращает или задает значение номера.
+		/// </summary>
+		public long Number
+		{
+			get
+			{
+				return this._number;
+			}
+			set
+			{
+				if (value.ToString().Length > MAXDIGITCOUNT || !value.ToString().StartsWith("7"))
+				{
+					throw new ArgumentException();
+				}
+				this._number = value;
+			}
+		}
+
+		/// <summary>
+		/// Создает экземпляр PhoneNumber.
+		/// </param>
+		public PhoneNumber(long number)
+		{
+			this.Number = number;
+		}
+	}
+}
---
+++ src/ContactsApp/ContactsApp.Model/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp.Model
+{
+    /// <summary>
+    /// Проект.
+    /// </summary>
+    class Project
+    {
+        /// <summary>
+        /// Список всех контактов.
+        /// </summary>
+        public List<Contact> Contacts { set; get; } = new List<Contact>();
+    }
+}
