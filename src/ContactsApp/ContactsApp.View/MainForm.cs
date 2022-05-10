f457a56453f353e784959bc3420caa5cbbe0d528
VladimirOstr <vovuchaostrovsky@gmail.com>
2022-05-10 23:19:27 +0700

    добавил функции добавления и обновления

--- src/ContactsApp/ContactsApp.View/MainForm.cs
+++ src/ContactsApp/ContactsApp.View/MainForm.cs
@@ -7,11 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ContactsApp.Model;
 
 namespace ContactsApp.View
 {
     public partial class MainForm : Form
     {
+        private Project _project = new Project();
+
+        private void UpdateListBox()
+        {
+            ContactsListBox.Items.Clear();
+            foreach (var contact in _project.Contacts)
+            {
+                ContactsListBox.Items.Add(contact.Surname);
+            }
+        }
+
+        private void AddContact()
+        {
+            var randomNames = new List<string>
+            {
+                "Иван", "Петр", "Владимир", "Никита"
+            };
+            var randomSurnames = new List<string>
+            {
+                "Петров", "Иванов", "Путин", "Зайка"
+            };
+            var randomEmails = new List<string>
+            {
+                "petrov@mail.ru", "ivanov@mail.ru", "pop@mail.ru",
+                "pap@mail.ru"
+            };
+            Random random = new Random();
+            Contact contact = new Contact(
+                randomNames[random.Next(randomNames.Count)],
+                randomSurnames[random.Next(randomSurnames.Count)],
+                new PhoneNumber(79138040329),
+                new DateTime(random.Next(1900,2022),
+                random.Next(1,12), random.Next(1,28)),
+                randomEmails[random.Next(randomEmails.Count)],
+                new Random().Next().ToString());
+            _project.Contacts.Add(contact);
+        }
+
         public MainForm()
         {
             InitializeComponent();
@@ -60,8 +99,10 @@ namespace ContactsApp.View
 
         private void AddContactButton_Click(object sender, EventArgs e)
         {
-            ContactForm addContactForm = new ContactForm();
-            addContactForm.Show();
+            //ContactForm addContactForm = new ContactForm();
+            //addContactForm.Show();
+            AddContact();
+            UpdateListBox();
         }
 
         private void EditContactButton_Click(object sender, EventArgs e)
