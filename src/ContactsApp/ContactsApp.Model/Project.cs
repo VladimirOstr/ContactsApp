d44c1b8446b14c031bc60c1d3d4b7817268c958b
VladimirOstr <vovuchaostrovsky@gmail.com>
2022-06-22 16:25:52 +0700

    Добавил сортировку

--- src/ContactsApp/ContactsApp.Model/Project.cs
+++ src/ContactsApp/ContactsApp.Model/Project.cs
@@ -15,5 +15,22 @@ namespace ContactsApp.Model
         /// Список всех контактов.
         /// </summary>
         public List<Contact> Contacts { get; set; } = new List<Contact>();
-    }
+
+		/// <summary>
+		/// Возвращает список контактов отсортированных по фамилии.
+		/// </summary>
+		/// <returns></returns>
+		public List<Contact> SortContacts()
+		{
+			for (int i = 0; i < Contacts.Count; i++)
+			{
+				if (Contacts[i] == null)
+				{
+					Contacts.RemoveAt(i);
+				}
+			}
+			return Contacts.OrderBy(
+				contact => contact.Surname).ToList<Contact>();
+		}
+	}
 }
